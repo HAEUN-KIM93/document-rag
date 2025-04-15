47b1a266b720701c2208704ac182fed4375e61d8
haeun kim <hkim9789952145@gmail.com>
2025-04-15 03:44:11 -0400

    initial

---
+++ main.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import rag_chain
+import olefile
+
+import tempfile
+import asyncio
+import sys
+from tavily import TavilyClient
+TAVILY_API_KEY = st.secrets["TAVILY_API_KEY"]  # 또는 os.getenv("TAVILY_API_KEY")
+client = TavilyClient(api_key=TAVILY_API_KEY)
+def show_history():
+    for q,a in st.session_state.chat_history:
+            with st.expander(f'{q}'):
+                st.markdown(f'{st.session_state.uploaded_file}')
+                st.markdown(f"**Q:** {q}")
+                st.markdown(f"**A:** {a}")
+        
+st.title('Q&A PDF')
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history=[]
+if 'chain' not in st.session_state:
+    st.session_state.chain = None
+if 'uploaded_file' not in st.session_state:
+    st.session_state.uploaded_file=None
+if 'web_search' not in st.session_state:
+    st.session_state.web_search=None
+def clear():
+    st.session_state.chat_history = []
+    st.session_state.uploaded_file=None
+    st.session_state.chain = None
+
+def clear_input():
+    query = st.session_state["search_query"]
+    if query:
+        results = client.search(query=query, max_results=3)
+        st.session_state["search_results"] = results
+        st.session_state["search_query"] = ""
+with st.sidebar:
+    uploaded_file=st.file_uploader('uploading the file',type=['pdf','hwp','xlsx'])
+    
+    if uploaded_file and st.button('📥 Upload and Process'):
+        
+        if uploaded_file.name.lower().endswith('.pdf'):
+            type='pdf'
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                tmp_file.write(uploaded_file.read())
+               
+        elif uploaded_file.name.lower().endswith('.hwp'):
+            type='hwp'
+            hwp_text = rag_chain.extrct_text_from_hwp(uploaded_file.read())
+            with tempfile.NamedTemporaryFile(delete=False,suffix='.txt', mode="w", encoding="utf-8") as tmp_file:
+                tmp_file.write(hwp_text)
+        elif uploaded_file.name.lower().endswith('.xlsx'):
+            type='xlsx'
+            
+            with tempfile.NamedTemporaryFile(delete=False,suffix='.xlsx') as tmp_file:
+                tmp_file.write(uploaded_file.read())
+        
+
+                
+                
+        else:
+            st.error("지원하지 않는 파일 형식입니다.")
+            tmp_path = None
+        st.session_state.uploaded_file = uploaded_file.name
+        st.success('pdf processed')
+        tmp_path = tmp_file.name 
+        chain_obj=rag_chain.rag().rag_chain(tmp_path,type)
+        st.session_state.chain = chain_obj 
+  
+    search_query = st.text_input("🔍 검색", key="search_query")
+    
+    search=st.button('search',on_click=clear_input)
+    
+        
+if "search_results" in st.session_state:
+    with st.expander("검색 결과"):
+        for r in st.session_state["search_results"]["results"]:
+            st.markdown(f"**[{r['title']}]({r['url']})**  \n{r['content'][:150]}...")   
+                
+if st.session_state.chain:
+    question=st.chat_input('type the question?')
+    st.header(f'{st.session_state.uploaded_file}')
+    
+    if question:
+        # 🔥 이전 대화 context 정리
+        chat_context = "\n".join([f"Q: {q}\nA: {a}" for q, a in st.session_state.chat_history])
+        full_question = f"{chat_context}\nQ: {question}" if chat_context else question
+        with st.spinner("🧠 답변 생성 중..."):
+            result = st.session_state.chain.ask(full_question)
+            st.session_state.chat_history.append((question,result))
+            
+show_history()     
+
+if st.button("🧹 clear file"):
+    clear()
+     
+   
+    
+
+
+   
---
+++ rag_chain.py
@@ -0,0 +1,100 @@
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.chat_models import ChatOllama
+import olefile
+import pandas as pd
+import tempfile
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.docstore.document import Document
+from langchain_openai import ChatOpenAI
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.document_loaders import PyPDFLoader,TextLoader
+from langchain.vectorstores import FAISS
+def extrct_text_from_hwp(file_bytes):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".hwp") as f:
+        f.write(file_bytes)
+        hwp_path = f.name
+
+    if not olefile.isOleFile(hwp_path):
+        return "❌ 이 파일은 HWP 포맷이 아닙니다."
+
+    ole = olefile.OleFileIO(hwp_path)
+    if ole.exists("PrvText"):
+        content = ole.openstream("PrvText").read()
+        try:
+            return content.decode("utf-16")
+        except:
+            return "❌ UTF-16 디코딩 실패"
+    return "❌ 읽을 수 있는 텍스트가 없습니다."  
+def excel_sheets_to_documents(file):
+    xls = pd.ExcelFile(file)
+    docs = []
+
+    for sheet in xls.sheet_names:
+        df = xls.parse(sheet)
+        for _, row in df.iterrows():
+            content = f"[시트: {sheet}]\n" + "\n".join([f"{col}: {row[col]}" for col in df.columns])
+            docs.append(Document(page_content=content))
+    
+    return docs
+class rag:
+    def __init__(self):
+        self.model=ChatOllama(model='exaone3.5:7.8b')
+        self.embedding = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-en"  # 또는 "sentence-transformers/all-MiniLM-L6-v2"
+)
+        self.chat_history=[]
+        self.vectorstore=None
+    def rag_chain(self,file_name,type):
+        if type=='pdf':
+            loader=PyPDFLoader(file_name)
+            docs=loader.load()
+        elif type=='hwp':
+            loader = TextLoader(file_name, encoding="utf-8")
+            docs=loader.load()
+        elif type == 'xlsx':
+            docs = excel_sheets_to_documents(file_name)
+        
+        text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+        split_document=text_splitter.split_documents(docs)
+        self.vectorstore=FAISS.from_documents(
+    documents=split_document,
+    embedding=self.embedding
+) 
+
+        retriever=self.vectorstore.as_retriever()
+        prompt=PromptTemplate.from_template(self.get_template())
+        
+        self.chain=(
+        {"context":retriever,"question":RunnablePassthrough()}
+        | prompt
+        |self.model
+        |StrOutputParser()
+        )
+        return self
+    def ask(self,question):
+        
+
+        result=self.chain.invoke(question)
+        return result
+        
+    def get_template(self):
+        prompt="""you are helpful assistant for question-answering .
+        use the following pieces of retrieved context to answer the question and source like page.
+        if you don't know the answer ,just say you don't know.
+        PLEASE Answer in KOREAN
+        #Context:
+        {context}
+
+        #Question:
+        {question}
+
+        #Answer:       
+        
+        """
+        return prompt
+
+     
